81f9f453922fefd7a800ba11cd65baddd67801fc
mickelsonmichael <mickelsonmichael@gmail.com>
2020-08-24 08:01:01 -0600

    Initial creation of React app

---
+++ web/src/app/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+export default hot(module)(() => (
+  <div className="app">
+    <h1>Welcome to WheelDecide</h1>
+  </div>
+));
