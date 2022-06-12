c2fdfa088ec444004bbc5b28436f23e629f93787
mickelsonmichael <mickelsonmichael@gmail.com>
2022-06-12 09:58:07 -0600

    Minor style improvements

--- web/src/apex/LegendPortrait.jsx
+++ web/src/apex/LegendPortrait.jsx
@@ -8,7 +8,7 @@ const LegendPortrait = ({ onClick, banned, legend, picked, hovered }) => {
             className={`legend-wrapper${banned ? " banned" : " unbanned"}${picked ? " picked" : " notpicked"}${hovered ? " hovered" : " nothovered"}`}
             onClick={onClick}
         >
-            <img alt={legend} src={src} />
+            <img alt={legend} src={src} draggable="false" />
         </div>
     )
 };
